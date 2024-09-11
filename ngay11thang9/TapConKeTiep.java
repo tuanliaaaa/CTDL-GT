a7efe92e5a82a8cfb03f8872d3d244be94745a97
nhatt <nhattuan44t@gmail.com>
2024-09-11 11:00:15 +0700

    11/09/2024

---
+++ ngay11thang9/TapConKeTiep.java
@@ -0,0 +1,44 @@
+package ngay11thang9;
+
+import java.util.Scanner;
+
+public class TapConKeTiep {
+     public static void tapConKeTiep(int n,int k, int []a)
+    {
+       int i=k-1;
+       while(i>0&&a[i]==n-k+i+1)i--;  
+       if(i==0)
+       {
+            for(int j=1;j<k+1;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+            return;
+       }
+       a[i]++;
+       for(int j=1+i;j<k;j++)
+       {
+        a[j]=a[j-1]+1;
+       }
+       for(int j=0;j<k;j++)
+       {
+            System.out.print(a[j]+" ");
+       }
+       System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t;
+        int n,k;
+        t=sc.nextInt();
+        while(t-->0)
+        {
+            n=sc.nextInt();
+            k=sc.nextInt();
+            int []a = new int[k];
+            for(int i=0;i<k;i++)a[i]=sc.nextInt();
+            tapConKeTiep(n,k,a);
+        }
+    }
+}
---
+++ ngay11thang9/XauNhiPhanKeTiep.java
@@ -0,0 +1,29 @@
+package ngay11thang9;
+
+import java.util.Scanner;
+
+public class XauNhiPhanKeTiep {
+    public static void xauNhiPhanKeTiep(String xau)
+    {
+        StringBuffer buffer = new StringBuffer(xau);
+        int i=buffer.length()-1;
+        while(buffer.charAt(i)=='1'&i>0)i--;
+        if(i!=0)buffer.setCharAt(i,'1');
+        else buffer.setCharAt(i, '0');
+        for(int j=i+1;j<buffer.length();j++)
+        {
+            buffer.setCharAt(j, '0');
+        }
+        System.out.println(buffer);
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t;
+        t=sc.nextInt();
+        sc.nextLine();
+        while(t-->0)
+        {
+            xauNhiPhanKeTiep(sc.nextLine());
+        }
+    }
+}
