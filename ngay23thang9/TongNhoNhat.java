514914f91c65c12baf22e1fcf2707bfda9d3ec25
nhatt <nhattuan44t@gmail.com>
2024-10-02 20:35:59 +0700

    23092024

--- ngay23thang9/TongNhoNhat.java
+++ ngay23thang9/TongNhoNhat.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class TongNhoNhap {
+public class TongNhoNhat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
