af9c1a3ebfc11d796da2b4402b85c9fd6379930e
nhatt <nhattuan44t@gmail.com>
2024-10-02 20:24:54 +0700

    28092024

--- ngay28thang9/SapXepCongViec2.java
+++ ngay28thang9/SapXepCongViec2.java
@@ -1,5 +1,29 @@
 package ngay28thang9;
 
+import java.util.*;
+
 public class SapXepCongViec2 {
-    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0)
+        {
+            int n=sc.nextInt();
+            Map<Integer,Integer> a= new HashMap<>();
+            for(int i=0;i<n;i++)
+            {
+                sc.nextInt();
+                int k=sc.nextInt();
+                int l=sc.nextInt();
+                int p=a.getOrDefault(k, 0);
+                a.put(k,Math.max(l,p));
+            }
+            Long sum=0L;
+            for (Map.Entry<Integer, Integer> entry : a.entrySet()) {
+               sum+=entry.getValue();
+            }
+            System.out.println(a.size()+" "+sum);
+        }
+    }
 }
+
