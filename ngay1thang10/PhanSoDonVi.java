caf2101996b50b4d227b0f667422f9ece9433911
nhatt <nhattuan44t@gmail.com>
2024-10-03 10:12:23 +0700

    01102024

--- ngay1thang10/PhanSoDonVi.java
+++ ngay1thang10/PhanSoDonVi.java
@@ -1,5 +1,25 @@
 package ngay1thang10;
 
+import java.util.Scanner;
+
 public class PhanSoDonVi {
-    
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0)
+        {
+            Long a=sc.nextLong();
+            Long b=sc.nextLong();
+            while(a!=0)
+            {
+              Long x=(a+b-1)/a;
+              
+              System.out.print("1/"+x+" ");
+              a=a*x-b;
+              b=b*x;
+              
+            }
+            System.out.println();
+        }
+    }
 }
