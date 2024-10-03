947313ea23660251f0dbd2fade5fe5c54a5ff9c1
nhatt <nhattuan44t@gmail.com>
2024-10-03 11:05:20 +0700

    30092024

--- ngay30thang9/SoKhoiLapPhuong.java
+++ ngay30thang9/SoKhoiLapPhuong.java
@@ -1,15 +1,39 @@
 package ngay30thang9;
 
+
 import java.util.Scanner;
+import java.util.*;
 
 public class SoKhoiLapPhuong {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int t=sc.nextInt();
-        while (t-->0)
-        {
-            int n=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); 
+        
+        while (T-- > 0) {
+            String N = sc.next(); 
+            long maxCube = -1;    
+
+            int len = N.length();
+            for (int i = 1; i < (1 << len); i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < len; j++) {
+                    if ((i & (1 << j)) != 0) {
+                        sb.append(N.charAt(j));
+                    }
+                }
                 
+                if (sb.length() > 0) {
+                    long num = Long.parseLong(sb.toString());
+                    long cubeRoot = Math.round(Math.cbrt(num));
+                    if (cubeRoot * cubeRoot * cubeRoot == num) {
+                        maxCube = Math.max(maxCube, num);
+                    }
+                }
+            }
+            
+            // In ra kết quả
+            System.out.println(maxCube);
         }
+        sc.close();
     }
 }
--- ngay30thang9/SoNhoNhat.java
+++ ngay30thang9/SoNhoNhat.java
@@ -3,7 +3,7 @@ package ngay30thang9;
 import java.util.Scanner;
 
 public class SoNhoNhat {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc= new Scanner(System.in);
         int t=sc.nextInt();
         while (t-->0)
@@ -13,16 +13,20 @@ public class SoNhoNhat {
             k=sc.nextInt();
             String a="";
             int count=0;
-            int i=9;
-            while(a.length()!=k)
+            int i=1;
+            Boolean check=true;
+            while(a.length()!=k&&i<10)
             {
-                
-                if((k-a.length()-1)+count+i<=n)
+                if(a.length()==1&&check){
+                    i=0;
+                    check=false;
+                }
+                if((k-a.length()-1)*9+count+i>=n)
                 {
-                    a=String.valueOf(i)+a;
+                    a=a+String.valueOf(i);
                     count+=i;
                 }else{
-                    i--;
+                    i++;
                 }
             }    
             System.out.println(a);
