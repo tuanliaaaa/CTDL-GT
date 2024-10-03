ee84eefc1311e415c259eaff2e63fd2bb1970dfa
nhatt <nhattuan44t@gmail.com>
2024-10-03 09:44:52 +0700

    29092024

--- ngay29thang9/NoiDay2.java
+++ ngay29thang9/NoiDay2.java
@@ -4,21 +4,22 @@ public class NoiDay2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int t= sc.nextInt();
+        Long l= 1000000007L;
         while (t-->0)
         {
             int n=sc.nextInt();
-            int []a= new int[n];
-            for(int i=0;i<n;i++)a[i]=sc.nextInt();
+            Long []a= new Long[n];
+            for(int i=0;i<n;i++)a[i]=sc.nextLong();
             Arrays.sort(a);
-            int sum=0;
+            Long sum=0l;
              while (n > 1) {
                 Arrays.sort(a, 0, n);
                 
-                int first = a[0];
-                int second = a[1];
+                Long first = a[0];
+                Long second = a[1];
                 
-                int newLength = first + second; 
-                sum += newLength;        
+                Long newLength = first + second; 
+                sum =sum%l+ newLength%l;        
                 
                 a[0] = newLength;
                 for (int i = 1; i < n - 1; i++) {
@@ -28,7 +29,7 @@ public class NoiDay2 {
                 n--; 
             }
             
-            System.out.println(sum);
+            System.out.println(sum%l);
         }
     }
 }
