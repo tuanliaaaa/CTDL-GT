c981f26f2e1a097cbf056e122b2046801a020014
nhatt <nhattuan44t@gmail.com>
2024-10-08 10:55:21 +0700

    08102024

---
+++ ngay8thang10/TichHaiSoNhiPhan.java
@@ -0,0 +1,17 @@
+package ngay8thang10;
+
+import java.util.Scanner;
+
+public class TichHaiSoNhiPhan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0)
+        {
+            String a=sc.next();
+            String b=sc.next();
+            System.out.println(a);
+            System.out.println(b);    
+        }
+    }
+}
