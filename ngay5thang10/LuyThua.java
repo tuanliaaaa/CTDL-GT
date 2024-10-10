03bd509f61d7fca097c36cb5082750a3f0587d9a
nhatt <nhattuan44t@gmail.com>
2024-10-10 10:04:44 +0700

    05102024

--- ngay5thang10/LuyThua.java
+++ ngay5thang10/LuyThua.java
@@ -3,11 +3,12 @@ package ngay5thang10;
 import java.util.Scanner;
 
 public class LuyThua {
+    private static Long l=1000000007L;
     public static Long luyThua(int n,int k) {
         if(k==0)return 1L;
-        Long m=luyThua(n, k/2);
-        if(k%2==0)return m*m;
-        else return m*m*n;
+        Long m=luyThua(n, k/2)%l;
+        if(k%2==0)return (m*m)%l;
+        else return (((m*m)%l)*(n%l))%l;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
